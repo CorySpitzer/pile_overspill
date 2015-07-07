9b8cba4818f672cad50674e72ec5744ab5f88c08
Cory Spitzer & Courtney Phillips <student@epicodus.com>
2015-07-07 14:57:33 -0700

    Pass integration testing for adding answer path.

--- spec/features/add_answer_page_spec.rb
+++ spec/features/add_answer_page_spec.rb
@@ -10,11 +10,18 @@ describe 'the answering a question path' do
     expect(page).to have_content "Content"
   end
 
-  # it 'adds a users answer to a question' do
-  #   user = set_current_user
-  #   question = FactoryGirl.create :question
-  #   fill_in 'Content', :with => "Test answer"
-  #   click_button "Add Answer"
-  #
-  # end
+  it 'adds a users answer to a question' do
+    user = set_current_user
+    click_link "Post Question"
+    fill_in 'Heading', :with=> "Test Question"
+    fill_in "Content", :with=> "Test Content"
+    click_button 'Add Question'
+    click_link 'Add Answer'
+    # question = FactoryGirl.create :question
+    # binding.pry
+    # visit new_answer_path(question)
+    fill_in 'Content', :with => "Test answer"
+    click_button "Add Answer"
+    expect(page).to have_content "Test answer"
+  end
 end
